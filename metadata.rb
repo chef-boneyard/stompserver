5ba1ae49b148cb0ddcda447cac1bb6e37c376400
Tim Smith <tsmith84@gmail.com>
2015-09-08 15:27:07 -0700

    Add long_description and supermarket metadata

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,7 @@ maintainer        "Chef Software, Inc."
 maintainer_email  "cookbooks@chef.io"
 license           "Apache 2.0"
 description       "Installs stompserver and sets up a service"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "1.0.1"
 
 recipe "stompserver", "Installs stompserver and starts the service"
@@ -10,3 +11,6 @@ recipe "stompserver", "Installs stompserver and starts the service"
 %w{ ubuntu debian centos fedora redhat }.each do |os|
   supports os
 end
+
+source_url 'https://github.com/chef-cookbooks/stompserver' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/stompserver/issues' if respond_to?(:issues_url)
